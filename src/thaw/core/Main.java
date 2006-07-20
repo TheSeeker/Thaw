d0c5f1b201e619ccbfa35bf181bb7fd1ad405ba3
Florent Daignière <nextgens@freenetproject.org>
2006-07-20 22:22:29 +0000

    new buildscript for thaw
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/Thaw@9683 67a373e5-eb02-0410-a15c-ee090a768436

--- src/thaw/core/Main.java
+++ src/thaw/core/Main.java
@@ -13,7 +13,7 @@ import thaw.i18n.I18n;
  */
 public class Main {
 
-	public final static String VERSION="0.2 Beta $Rev$".replaceAll("\\$", "");
+	public final static String VERSION="0.2 Beta r@custom@";
 
 	/**
 	 * Look & feel use by GUI front end
