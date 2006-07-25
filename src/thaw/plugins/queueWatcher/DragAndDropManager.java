0fc414c2a62cb037aeb1c4ee3a204bacb1fabee0
Jerome Flesch <jflesch@gmail.com>
2006-07-25 14:11:45 +0000

    Fix a mistake in drag'n'drop
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/Thaw@9747 67a373e5-eb02-0410-a15c-ee090a768436

--- src/thaw/plugins/queueWatcher/DragAndDropManager.java
+++ src/thaw/plugins/queueWatcher/DragAndDropManager.java
@@ -43,10 +43,10 @@ public class DragAndDropManager implements DragGestureListener, DragSourceListen
 		dragSource = DragSource.getDefaultDragSource();
 
 		for(int i = 0 ; i < queuePanels.length ; i++) {
-			/* this.dragSource.createDefaultDragGestureRecognizer(queuePanels[i].getTable(),
+			this.dragSource.createDefaultDragGestureRecognizer(queuePanels[i].getTable(),
 			                                                      DnDConstants.ACTION_COPY_OR_MOVE,
 									      this);
-			*/
+			
 			//queuePanels[i].getTable().setTransferHandler(new FileTransferHandler());
 			//queuePanels[i].getTable().setDragEnabled(true);
 
