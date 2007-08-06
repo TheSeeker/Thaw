8d51ace0f95f721404c28b6689ba00448e3a10d3
Jerome Flesch <jflesch@gmail.com>
2007-08-06 17:00:16 +0000

    Add the publicly writable index to the defaults indexes
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/Thaw@14491 67a373e5-eb02-0410-a15c-ee090a768436

--- src/thaw/plugins/IndexBrowser.java
+++ src/thaw/plugins/IndexBrowser.java
@@ -20,10 +20,16 @@ import thaw.plugins.index.IndexManagementHelper;
 public class IndexBrowser extends ToolbarModifier implements Plugin, ChangeListener {
 
 	public static final String[] DEFAULT_INDEXES = new String[] {
-			"USK@p-uFAWUomLm37MCQLu3r67-B8e6yF1kS4q2v0liM1Vk,"+
-				"h0MWqM~lF0Bec-AIv445PLn06ams9-RFbnwO6Cm2Snc,AQACAAE"+
-				"/Thaw/2/Thaw.frdx"
-		};
+		/* publicly writable index */
+		"USK@61m2WMJEA9pyQQQ-hjGN8lIM2xToNJHyacJ8ZPB9JCQ,"+
+		"5aEPJBhwIV~HpGIG8YTpKSB39WCGgd0BUNWZ012745Y,AQACAAE"+
+		"/Publicly%20writable%20index/35/Publicly writable index.frdx",
+
+		/* Thaw index */
+		"USK@p-uFAWUomLm37MCQLu3r67-B8e6yF1kS4q2v0liM1Vk,"+
+		"h0MWqM~lF0Bec-AIv445PLn06ams9-RFbnwO6Cm2Snc,AQACAAE"+
+		"/Thaw/2/Thaw.frdx"
+	};
 
 	private Core core;
 	private Hsqldb hsqldb;
